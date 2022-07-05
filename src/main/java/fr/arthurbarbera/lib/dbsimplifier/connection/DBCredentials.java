55ea9072fa62c62b109c4f254cdf83c5eac7cdff
arthur-barbera <arthurbarbera5@gmail.com>
2022-07-05 15:24:12 +0200

    Clearing repository to put Maven repository

--- src/main/java/fr/arthurbarbera/lib/dbsimplifier/connection/DBCredentials.java
+++
@@ -1,9 +0,0 @@
-package fr.arthurbarbera.lib.dbsimplifier.connection;
-
-public record DBCredentials(String host, String databaseName, String user, String pass, int port) {
-
-    public String toURI() {
-        return "jdbc:mysql://" + (this.host.isBlank() ? "localhost" : host) + ":" + this.port + "/" + this.databaseName;
-    }
-
-}
